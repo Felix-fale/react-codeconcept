03b3f939955cdb3746e9ea0fabb59856b20bb613
Felix-fale <falefelix23@gmail.com>
2022-11-25 20:33:40 +0000

    first commit

---
+++ src/components/Menu.js
@@ -0,0 +1,11 @@
+export default function Menu(parms) {
+    return(
+        <div className="menu">
+            <ul>
+                <li>Home</li>
+                <li>Add Techno 18m00</li>
+                <li>All Techno</li>
+            </ul>
+        </div>
+    )
+}
